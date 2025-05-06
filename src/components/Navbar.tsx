25976933bdb00e83f99e6446878501980f07cd18
Joe Reeve <joe.reeve@gmail.com>
2025-05-06 23:03:36 +0900

    feat: add a transition when toggling menu on mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import Link from "next/link";
 import { Menu, X } from "lucide-react";
+import clsx from "clsx";
 
 import { Button } from "./ui/button";
 
@@ -36,8 +37,13 @@ function MobileNavbar() {
       >
         {menuToggled ? <X /> : <Menu />}
       </Button>
-      {menuToggled && (
-        <nav className="absolute top-12 left-0 w-screen h-[calc(100vh-3rem)] bg-green-200">
+      {
+        <nav
+          className={clsx(
+            "absolute top-12 left-0 w-screen h-[calc(100vh-3rem)] bg-green-200 transition duration-300 ease-in-out",
+            { "-z-10 opacity-0": !menuToggled, "-z10 opacity-100": menuToggled }
+          )}
+        >
           <ul className="flex-col gap-4">
             <li>
               <Link href="/">Link 1</Link>
@@ -50,7 +56,7 @@ function MobileNavbar() {
             </li>
           </ul>
         </nav>
-      )}
+      }
     </div>
   );
 }
